8a966b4e8565623e74fa7e3fcf5d6fbb84f70f6b
minho.Kim <minho0078@gmail.com>
2016-05-19 16:26:17 +0900

    commit

---
+++ js/vp.js
@@ -0,0 +1,62 @@
+var vp = "";
+var vs_video = function(){
+                  this.speedUp = function(obj){
+                     vp = document.getElementById(obj);
+                     vp.playbackRate += 0.5;
+                  }
+                  ,this.speedDown = function(obj){
+                     vp = document.getElementById(obj);
+                     vp.playbackRate -= 0.5;
+                  }
+                  ,this.volumeUp = function(obj){
+                     vp = document.getElementById(obj);
+                     if(vp.volume < 1){
+                        vp.volume += 0.1;
+                     }
+                  }
+                  ,this.volumeDown = function(obj){
+                     vp = document.getElementById(obj);
+                     if(vp.volume >0){
+                        vp.volume -= 0.1;
+                     }
+                  }
+               }
+
+
+
+// 자막 DIV 위치 세팅~
+var oSelf = $('.sub');
+var oSelfArray = oSelf.attr('id').split("_");
+var oBoss = $('#'+oSelfArray[0]);
+
+var selfId = oSelf.attr('id');
+var subDiv = $('#'+selfId);
+
+subDiv.css(
+   {  'width': oBoss.css('width')   }
+);
+
+subDiv.position().top = oBoss.position().top;
+
+var vPosition = getAbsolutePos(document.getElementById(oSelfArray[0]));
+var sPosition = getAbsolutePos(document.getElementById(oSelf.attr('id')));
+
+var new_sPostion_y = vPosition.x + parseInt(oBoss.css('height').replace("px","")) - parseInt(oSelf.css('height').replace("px",""));
+
+subDiv.offset({top: new_sPostion_y-40});
+
+function getAbsolutePos(obj) {
+	var position = new Object;
+	position.x = 0;
+	position.y = 0;
+	if( obj ) {
+		position.x = obj.offsetLeft + obj.clientLeft;
+		position.y = obj.offsetTop + obj.clientTop;
+		if( obj.offsetParent ) {
+			var parentpos = getAbsolutePos(obj.offsetParent);
+			position.x += parentpos.x;
+			position.y += parentpos.y;
+		}
+	}
+	return position;
+}
